5397582da0ba413610c409d0bc47103675ae6039
Eduardo98c <edyautore@gmail.com>
2021-12-08 01:20:20 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_app():
+    return render_template('HomeTemplate.html')
+
+
+@app.route('/LoginTemplate.html')
+def login():
+    return render_template('LoginTemplate.html')
+
+
+@app.route('/RegistrationTemplate.html')
+def registration():
+    return render_template('RegistrationTemplate.html')
+
+
+if __name__ == '__main__':
+    app.run()
